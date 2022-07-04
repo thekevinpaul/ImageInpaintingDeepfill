df2b753462bc9e93c2fbf7a909a420d5f6609f18
Amizhthan+Kevin <103363876+FYP-2022-DrSridhar@users.noreply.github.com>
2022-07-04 09:10:45 +0530

    Update inpaint.py

--- inpaint.py
+++ inpaint.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader
 
 from config import *
 from deepfillv2 import test_dataset
-from deepfillv2 import latent
+#from deepfillv2 import latent
 from deepfillv2 import utils
 from test import latent
 
